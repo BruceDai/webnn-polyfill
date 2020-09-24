a60db8e9503e7b937eaac57559878374a2b436f4
RuiBao <dairui@outlook.com>
2020-09-24 20:28:28 +0800

    test by adding test

--- test/ops/add.js
+++ test/ops/add.js
@@ -165,4 +165,19 @@ describe('test add', function() {
     ];
     checkOutput(outputBuffer, expected);
   });
+  it('add test converted from Android CTS', async function() {
+    const op1 = nn.input('op1', {type: 'tensor-float32', dimensions: [2]});
+    const op2 = nn.input('op2', {type: 'tensor-float32', dimensions: [2]});
+    const expected = [4.0, 6.0];
+    const op3 = nn.add(op1, op2);
+    const model = await nn.createModel([{name: 'op3', operand: op3}]);
+    const compilation = await model.createCompilation();
+    const execution = await compilation.createExecution();
+    execution.setInput('op1', new Float32Array([1.0, 2.0]));
+    execution.setInput('op2', new Float32Array([3.0, 4.0]));
+    const outputBuffer = new Float32Array(expected.length);
+    execution.setOutput('op3', outputBuffer);
+    await execution.startCompute();
+    checkOutput(outputBuffer, expected);
+  });
 });
